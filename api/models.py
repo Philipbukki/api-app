0e5cf3ec55656f13b26d363824da44169b53acab
Philip Bukii <philipbukkie@gmail.com>
2020-11-30 10:59:06 +0300

    made some edits to the codes

--- api/models.py
+++ api/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
+
 class Variable(models.Model):
     name = models.CharField(max_length=32)
     value = models.IntegerField()
+
     def __str__(self):
         return self.name
+
 
 class Member(models.Model):
     name = models.CharField(max_length=32)
     is_son = models.BooleanField(default=False)
-    household_count =models.IntegerField()
-
+    household_count = models.IntegerField()
 
     def __str__(self):
         return self.name
-
-
-
-
-
--- api/schema.py
+++ api/schema.py
@@ -2,58 +2,66 @@ from graphene_django.types import DjangoObjectType
 import graphene
 from .models import Member, Variable
 
+
 class MemberType(DjangoObjectType):
     class Meta:
-        model =Member
+        model = Member
+
 
 class VariableType(DjangoObjectType):
     class Meta:
         model = Variable
+
+
 class Query(graphene.ObjectType):
-    all_variables =graphene.List(VariableType)
-    all_members =graphene.List(MemberType)
+    all_variables = graphene.List(VariableType)
+    all_members = graphene.List(MemberType)
+
     def resolve_all_variables(self, info, **kwargs):
         return Variable.objects.all()
+
     def resolve_all_members(self, info, **kwargs):
         return Member.objects.all()
 
+
 class MemberCreateMutation(graphene.Mutation):
     member = graphene.Field(MemberType)
-    class Arguments:
-        name =graphene.String(required=True)
-        is_son = graphene.Boolean(required=True)
-        household_count =graphene.Int(required=True)
 
+    class Arguments:
+        name = graphene.String(required=True)
+        is_son = graphene.Boolean(required=True)
+        household_count = graphene.Int(required=True)
 
     def mutate(self, info, name, is_son, household_count):
-
-        member = Member.objects.create(name=name, is_son=is_son, household_count=household_count)
+        member = Member.objects.create(
+            name=name, is_son=is_son, household_count=household_count)
         member.save()
-
 
         return MemberCreateMutation(member=member)
 
+
 class VariableCreateMutation(graphene.Mutation):
     variable = graphene.Field(VariableType)
+
     class Arguments:
-        name =graphene.String(required=True)
+        name = graphene.String(required=True)
         value = graphene.Int(required=True)
 
     def mutate(self, info, name, value):
-
         variable = Variable.objects.create(name=name, value=value)
         variable.save()
 
-
         return VariableCreateMutation(variable=variable)
+
 
 class MemberUpdateMutation(graphene.Mutation):
     member = graphene.Field(MemberType)
+
     class Arguments:
-        name =graphene.String()
+        name = graphene.String()
         is_son = graphene.Boolean()
-        household_count =graphene.Int()
-        id =graphene.ID(required=True)
+        household_count = graphene.Int()
+        id = graphene.ID(required=True)
 
     def mutate(self, info, name, is_son, household_count, id):
 
@@ -61,26 +69,24 @@ class MemberUpdateMutation(graphene.Mutation):
         if name is not None:
             member.name = name
         if household_count is not None:
-            member.household_count= household_count
+            member.household_count = household_count
         if is_son is not None:
-            member.is_son =is_son
+            member.is_son = is_son
 
         member.save()
 
         return MemberUpdateMutation(member=member)
 
+
 class VariableUpdateMutation(graphene.Mutation):
     variable = graphene.Field(VariableType)
-
 
     class Arguments:
         name = graphene.String()
         value = graphene.Int()
         id = graphene.ID(required=True)
 
-
     def mutate(self, info, name, value, id):
-
 
         variable = Variable.objects.get(pk=id)
         if name is not None:
@@ -90,16 +96,16 @@ class VariableUpdateMutation(graphene.Mutation):
 
         variable.save()
 
-        return VariableUpdateMutation(variable =variable)
+        return VariableUpdateMutation(variable=variable)
 
 
 class MemberDeleteMutation(graphene.Mutation):
     member = graphene.Field(MemberType)
+
     class Arguments:
-        id =graphene.ID(required=True)
+        id = graphene.ID(required=True)
 
     def mutate(self, info, id):
-
         member = Member.objects.get(pk=id)
         member.delete()
 
@@ -108,16 +114,15 @@ class MemberDeleteMutation(graphene.Mutation):
 
 class VariableDeleteMutation(graphene.Mutation):
     variable = graphene.Field(VariableType)
+
     class Arguments:
-        id =graphene.ID(required=True)
+        id = graphene.ID(required=True)
 
     def mutate(self, info, id):
-
         variable = Variable.objects.get(pk=id)
         variable.delete()
 
         return VariableDeleteMutation(variable=None)
-
 
 
 class Mutation(graphene.ObjectType):
@@ -127,7 +132,3 @@ class Mutation(graphene.ObjectType):
     create_variable = VariableCreateMutation.Field()
     update_variable = VariableUpdateMutation.Field()
     delete_variable = VariableDeleteMutation.Field()
-
-
-
-
